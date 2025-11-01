9f91df04f0992d45b6f07af38082c33e33c5f360
Abeni <abenezerferede3@gmail.com>
2025-11-01 04:42:50 +0300

    feat: add video list page

---
+++ lib/screens/video_list_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:msp_mobile/widgets/card.dart';
+
+class VideoListPage extends StatefulWidget {
+  final String title;
+  final List<Map<String, dynamic>> videos;
+
+  const VideoListPage({
+    Key? key,
+    required this.title,
+    required this.videos,
+  }) : super(key: key);
+
+  @override
+  State<VideoListPage> createState() => _VideoListPageState();
+}
+
+class _VideoListPageState extends State<VideoListPage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        
+      ),
+      body: _buildListView(),
+    );
+  }
+
+  Widget _buildListView() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(5),
+      itemCount: widget.videos.length,
+      itemBuilder: (context, index) {
+        final video = widget.videos[index];
+        return Container(
+          margin: const EdgeInsets.only(bottom: 12),
+          child: VideoThumbnailCard(
+            thumbnailUrl: video['thumbnailUrl'],
+            duration: video['duration'],
+            title: video['title'],
+            subtitle: video['subtitle'],
+            avatarUrl: video['avatarUrl'],
+            onTap: () {
+              // Handle video tap
+              print('Tapped on: ${video['title']}');
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  
+}
